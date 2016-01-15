63057dda60e385786b76e350907fe94a5d962bec
maticaputti <matias.caputti@gmail.com>
2016-01-15 10:41:14 -0300

    Add Mandrill config

---
+++ sources/optin.conf.ts
@@ -0,0 +1,10 @@
+module.exports = {
+    mandrill: {
+        key: 'bnbWeBxHtOxrHmGRnj2mQQ',
+
+        adminEmail: 'epotignano@gmail.com',
+        adminName: 'Emiliano Potignano',
+        userEmail: 'epotignano@gmail.com',
+        userName: 'Emiliano Potignano'
+    }
+};
